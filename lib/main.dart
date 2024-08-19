292456344d048566b5ee1586a50d121d7dccb5cd
basilbab <basilbab@gmail.com>
2024-08-19 21:30:32 +0530

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,114 @@ class ScreenProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text(
-        'Personal Profile',
-        style: TextStyle(
-            color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        //AppBar starts here
+        title: const Text(
+          'Personal Profile',
+          style: TextStyle(
+              color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.pink,
       ),
-      backgroundColor: Colors.pink,
-    ));
+      //App Bar ends here
+      //Scafold Body Starts here
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(children: [
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text('Name'),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Type your name',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15)))),
+                  ),
+                ),
+              ),
+            ]),
+            Row(children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Email'),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Type your Email',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15)))),
+                  ),
+                ),
+              ),
+            ]),
+            Row(children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Mobile'),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Type your Mobile Number',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15)))),
+                  ),
+                ),
+              ),
+            ]),
+            Row(children: [
+              const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Text('Address'),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Type your Address',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15)))),
+                    maxLines: 5,
+                  ),
+                ),
+              ),
+            ]),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Your onPressed function here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink, // Background color
+                  foregroundColor: Colors.white, // Text color
+                ),
+                child: Text('Submit'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
